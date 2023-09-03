26fe0b65efc83dd504c8dccf8a3fdc54ffc60e61
lauraalvarezzz1 <lauraalvarez.mgvc@gmail.com>
2023-09-03 17:21:38 +0200

    shared module and set up token to get services (initial configuration)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   avantioAPIHost: 'https://challenge.avantio.pro',
-  avantioAPIAuthToken: '',
+  avantioAPIAuthToken: 'hsilqiyebbqs7w24kjnpn',
 };
 
 /*
